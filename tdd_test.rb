f472d3ea5a9d49992fa637c44f9c7d3c4f1cdf76
Ricky Collins <richardcollins52317@gmail.com>
2018-06-20 15:38:13 -0400

    test for 8 inch pass

--- tdd_test.rb
+++ tdd_test.rb
@@ -10,4 +10,9 @@ class TestTddPizzaApp < Minitest::Test
     def test_for_hash
         assert_equal(Hash, get_order().class)
     end
+
+    def test_for_8_inch
+        assert_equal(7.00, get_order()["8 inch"])
+    end
+
 end
