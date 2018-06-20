f25e276f9958b15ca9a64424de113177d701f6d9
Ricky Collins <richardcollins52317@gmail.com>
2018-06-20 15:31:01 -0400

    first test

---
+++ tdd_test.rb
@@ -0,0 +1,10 @@
+require "minitest/autorun"
+require_relative "tdd_pizza_app.rb"
+
+class TestTddPizzaApp < Minitest::Test
+
+    def test_assert_that_1_equals_1
+        assert_equal(1, 1)
+    end
+
+end
